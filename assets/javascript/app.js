5ecb4c1d8cd851b4269ec21306ce1477880eac57
Ryan Bregier <rabunc@gmail.com>
2017-06-30 15:17:40 -0400

    trying to get evaluateAnswers() working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var currentQuestion = 0;
 
     var triviaObjectArray = [
-            {
+        {
             question: "How many National Championships has UNC won?",
-            answerOptions: ["4","9","6","3"],
+            answerOptions: ["4", "9", "6", "3"],
             // "correct answer position" counting from 0, i.e. [2] is the 3rd option
-            correctAnswerPosition: 2 
+            correctAnswerPosition: 2
         },
-            {
+        {
             question: "How many NBA Championships did Michael Jordan win?",
-            answerOptions: ["6","2","5","8"],
-            correctAnswerPosition: 0 
+            answerOptions: ["6", "2", "5", "8"],
+            correctAnswerPosition: 0
         },
-            {
+        {
             question: "What is the name of the main character of Burn Notice?",
-            answerOptions: ["Michael Scott","Michael Westen","Jason Bourne","Jason Stallman"],
+            answerOptions: ["Michael Scott", "Michael Westen", "Jason Bourne", "Jason Stallman"],
             correctAnswerPosition: 1
         }
     ]
 
-    function generateTriviaAnswers() {
-        var answers = triviaObjectArray[currentQuestion].answerOptions;
-        for (i=0;i<answers.length;i++) {
-            var newAnswersDiv = $("<div>");
-            $(newAnswersDiv).addClass("answer-options");
-            $(newAnswersDiv).text(answers[i]);
-            $("#answers-area").append(newAnswersDiv);
-        }
-        currentQuestion++;
-    }
-
     function generateTriviaQuestion() {
         var newQuestionSpan = $("<span>");
-        var question
         // Pull question from triviaObjectArray sequentially
         newQuestionSpan.text(triviaObjectArray[currentQuestion].question);
         console.log(triviaObjectArray[currentQuestion].question);
         $("#question").append(newQuestionSpan);
     }
 
-    $("#start-button").on("click", function() {
+    function generateTriviaAnswers() {
+        var answers = triviaObjectArray[currentQuestion].answerOptions;
+        for (i = 0; i < answers.length; i++) {
+            var newAnswersDiv = $("<button>");
+            $(newAnswersDiv).addClass("answer-options");
+            $(newAnswersDiv).attr("value", answers[i]);
+            $(newAnswersDiv).attr("position", [i]);
+            $(newAnswersDiv).text(answers[i]);
+            $("#answers-area").append(newAnswersDiv);
+        }
+    }
+
+    function evaluateAnswer() {
+        var answers = triviaObjectArray[currentQuestion].answerOptions;
+        var correctAnswer = answers[triviaObjectArray[currentQuestion].correctAnswerPosition];
+        console.log(correctAnswer)
+        console.log(this)
+        var selectedOption = $(this).attr("value");
+        console.log(selectedOption);
+        currentQuestion++;
+
+    }
+
+
+    $("#start-button").on("click", function () {
         $("#start-button").empty();
         generateTriviaQuestion();
         generateTriviaAnswers();
+    })
+
+    // When an answer option is clicked, check to see if it's correct, then show correct gif
+    $(document).on("click", ".answer-options", function () {
+        evaluateAnswer();
     })
 
 });
