73ca58538e59143ccb2515494d30d17bfc56e303
Ryan Bregier <rabunc@gmail.com>
2017-07-03 22:00:13 -0400

    play again button hooked up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -118,6 +118,10 @@ $(document).ready(function () {
         $("#correct").append("You got " + questionsRight + " questions correct!")
         $("#incorrect").append("You got " + questionsWrong + " questions wrong!")
         stopTimer();
+        var newStartButton = $("<button>");
+        newStartButton.text("Play Again")
+        newStartButton.addClass("start-button");
+        $("#start-button-area").append(newStartButton)
         return;
     }
 
@@ -134,7 +138,7 @@ $(document).ready(function () {
         var correctAnswer = answers[triviaObjectArray[currentQuestion].correctAnswerPosition];
         var confirmation = $("<div>");
         $(confirmation).addClass("#confirmation");
-        $(confirmation).text("The correct answer is " + correctAnswer + "!");
+        $(confirmation).text("The answer is " + correctAnswer + "!");
         console.log(confirmation);
         $("#confirmation-area").append(confirmation);
         // Add gif to confirmation
@@ -148,15 +152,33 @@ $(document).ready(function () {
     function clearConfirmation() {
         $("#confirmation-area").empty();
         $("#gif-area").empty();
+        $("#correctness-area").empty();
     }
 
     function clearOldText() {
         $("#question").empty();
         $("#answers-area").empty();
         $("#countdown-timer").empty();
+        
+    }
+
+    function announceOutOfTime() {
+        var correctness = $("<div>")
+        $(correctness).text("You're out of time!")
+        $("#correctness-area").append(correctness)
+        showConfirmation();
+        questionsWrong++;
+        currentQuestion++;
+        if (gameOver != true) {
+            setTimeout(generateTriviaQuestion, 3000);
+            setTimeout(generateTriviaAnswers, 3000);
+        }
     }
 
     function announceCorrect() {
+        var correctness = $("<div>")
+        $(correctness).text("That's correct!")
+        $("#correctness-area").append(correctness)
         showConfirmation();
         questionsRight++;
         currentQuestion++;
@@ -168,6 +190,9 @@ $(document).ready(function () {
     }
 
     function announceIncorrect() {
+        var correctness = $("<div>")
+        $(correctness).text("That's incorrect!")
+        $("#correctness-area").append(correctness)
         showConfirmation();
         questionsWrong++;
         currentQuestion++;
@@ -235,8 +260,14 @@ $(document).ready(function () {
         }
     }
 
-    $("#start-button").on("click", function () {
+    $(document).on("click", ".start-button", function () {
         $("#start-button-area").empty();
+        currentQuestion = 0;
+        gameOver = false;
+        clearOldText();
+        clearConfirmation();
+        $("#correct").empty()
+        $("#incorrect").empty()
         generateTriviaQuestion();
         generateTriviaAnswers();
     })
